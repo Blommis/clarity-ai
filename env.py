bbc6c1cbd9463f5c6cea5939aecc4b6507cbf716
Blommis <blomquist799@gmail.com>
2026-01-03 15:30:14 +0100

    Remove env.py from version control

--- env.py
+++
@@ -1,3 +0,0 @@
-import os
-os.environ.setdefault("SECRET_KEY", "django-insecure-hk^tuz_srk)*e077ien+gn4_v#zn@trafx*r^kw*#0m2+p^4(m")
-os.environ.setdefault("DATABASE_URL", "postgresql://neondb_owner:npg_rb5X7BxqhglG@ep-blue-lake-a2jdusph.eu-central-1.aws.neon.tech/reset_stank_plead_989951")
