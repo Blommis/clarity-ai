27617ff837f8aad1c4aa3f58713c4dbf18e17236
Blommis <blomquist799@gmail.com>
2026-01-03 15:03:50 +0100

    linked heroku account to the project

--- clarityai/settings.py
+++ clarityai/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    """ noqa is added to avoid false positive linting warnings. """
+    import env  # noqa:
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +27,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hk^tuz_srk)*e077ien+gn4_v#zn@trafx*r^kw*#0m2+p^4(m'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("DATABASE_URL")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '.127.0.0.1',]
+
 
 
 # Application definition
@@ -71,14 +80,21 @@ TEMPLATES = [
 WSGI_APPLICATION = 'clarityai.wsgi.application'
 
 
+
+
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default':
+dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
