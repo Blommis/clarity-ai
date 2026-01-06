9684234843221f781c1dc75325a8fba8d05a5d05
Blommis <blomquist799@gmail.com>
2026-01-06 13:31:19 +0100

    set to false in settings

--- clarityai/settings.py
+++ clarityai/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', 'localhost']
 
